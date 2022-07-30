f431898103204c3fe72d2f925f818b979ea8ae4d
Oleh Foia <oleganfoya@gmail.com>
2022-07-30 19:54:11 +0300

    new func show and create

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//Веб приложение через которое можно будет создавать и делится заметками.
+
 package main
 
 import (
@@ -5,15 +7,36 @@ import (
 	"net/http"
 )
 
+//Создается функция-обработчик домашней страницы "home"
 func home(w http.ResponseWriter, r *http.Request) {
+	//Проверка на catch-all
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Привет из Snippetbox"))
 }
 
+//Создается функция-обработчик домашней страницы "showSnippet"
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Выводи заметки из Snippetbox"))
+}
+
+//Создается функция-обработчик домашней страницы "creatSnippet"
+func creatSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Создаёт заметки в  Snippetbox"))
+}
+
 func main() {
+	//http.NewServeMux() для инициализации нового рутера
+	//Регистрируется как обработчик для URL-шаблонов.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", creatSnippet)
 
+	//Функция http.ListenAndServe() используется для запуска нового веб-сервера.
 	log.Println("Запуск веб-сервера http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe("127.0.0.1:4000", mux)
 	log.Fatal(err)
 }
