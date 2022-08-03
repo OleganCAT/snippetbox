35b542e171e18206f51c04c94c0760699e9a4842
Oleh Foia <oleganfoya@gmail.com>
2022-08-03 14:43:10 +0300

    added models

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,5 +51,15 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Создаёт заметки в  Snippetbox"))
+
+	title := "История про улитку"
+	content := "Улитка выползла из раковины,\nвытянула рожки,\nи опять подобрала их."
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
