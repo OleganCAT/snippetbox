72d5768245e5a2b0dd7ed79adb4a0ec88eba1174
Oleh Foia <oleganfoya@gmail.com>
2022-08-03 15:28:27 +0300

    SQL

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"golangify.com/snippetbox/pkg/models"
 	"html/template"
 	"net/http"
 	"strconv"
@@ -41,7 +43,16 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%v", s)
 }
 
 //Создается функция-обработчик для страницы "creatSnippet" которая отвечает только на POST запросы
